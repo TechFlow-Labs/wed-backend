b16c6fa8267f9ae950c3ebcee84aff6e16013919
StergiosFotiadis <stergiosfotiadis1@gmail.com>
2026-05-18 22:40:28 +0300

    testing n8n

--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -106,4 +106,7 @@ def update_my_profile(profile_update: UserProfileUpdateSchema, db: Session = Dep
         User.id == current_user.id
     ).first()
 
+
+    #test n8n !
+
     return updated_profile
